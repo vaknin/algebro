ce38fd9b8f1d3c9afcbe4c0292c8475f6f88ffb5
Kivan <vaknins33@gmail.com>
2019-02-02 18:44:12 +0200

    chord system is semi-working

--- script.js
+++ script.js
@@ -10,8 +10,7 @@ let streak = 1;
 let min = 3;
 let max = 13;
 let answer;
-let notes = [0, 2, 3, 4, 5 ,6 ,1];
-let playedNotes = [];
+let playedChords = [];
 
 //Listen for 'Enter' keypress
 document.addEventListener('keypress', function(event){
@@ -23,8 +22,6 @@ document.addEventListener('keypress', function(event){
 //Get a new exercise
 function getExercise(){
     input.value = "";
-    document.body.style.backgroundColor = `rgb(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)})`;
-    input.style.backgroundColor = `rgb(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)})`;
 
     //Multiply
     let n1 = Math.round(Math.random() * (max - min)) + min;
@@ -44,14 +41,12 @@ async function checkAnswer(){
         
         scoreNode.innerHTML = score;
         getExercise();
-        for (let i = 0; i < 7; i++) {
-            playNote();
-            await sleep(130);
-        }
+        playChord();
     }
 
     //Incorrect
     else{
+        document.body.style.backgroundColor = `rgb(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)})`;
         input.value = "";
         score = 0;
         streak = 1;
@@ -59,30 +54,31 @@ async function checkAnswer(){
     }
 }
 
-//Randomly play a note
-function playNote(){
-    let audio;
-
-    //If no notes left in the array, rebuild it
-    if (notes.length == 0){
-        notes = [0, 2, 3, 4, 5 ,6 ,1];
-        playedNotes = [];
+//Randomly play a chord
+function playChord(){
+    //If no chords left in the array, rebuild it
+    if (chords.length == 0){
+        initializeChords();
     }
 
-    //If the note has already been played, stay in loop
+    //If the chord has already been played, stay in loop
     while (true){
-        let n = notes[Math.round(Math.random() * (notes.length - 1))];
+        let n = chords[Math.round(Math.random() * (chords.length - 1))];
         
-        //Note hasn't been played yet
-        if (playedNotes.indexOf(n) == -1 && n != undefined){
-            
-            //Get the index of that note, and then remove it from the array, to not play it again
-            let index = notes.indexOf(n);
-            notes.splice(index, 1);
-            playedNotes.push(n);
-            //Play the note
-            audio = new Audio(`sounds/majorc/${n}.wav`);
-            audio.play();
+        //Chord hasn't been played yet
+        if (playedChords.indexOf(n) == -1 && n != undefined){
+            //Play the chord
+            let note1 = new Audio(`sounds/${n[0]}.wav`);
+            let note2 = new Audio(`sounds/${n[1]}.wav`);
+            let note3 = new Audio(`sounds/${n[2]}.wav`);
+            note1.play();
+            note2.play();
+            note3.play();
+
+            //Remove it from the array, to not play it again
+            let index = chords.indexOf(n);
+            chords.splice(index, 1);
+            playedChords.push(n);
             break;
         }
     }
@@ -92,6 +88,23 @@ function sleep(ms){
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function initializeChords(){
+
+    let Cmajor = ["C", "E", "G"];
+    let Dmajor = ["D", "Fs", "A"];
+    let Emajor = ["E", "Gs", "B"];
+    let Fmajor = ["F", "A", "C"];
+    let Gmajor = ["G", "B", "D"];
+    let Amajor = ["A", "Cs", "E"];
+    let Bmajor = ["B", "Eb", "Fs"];
+
+    chords = [Amajor, Bmajor, Cmajor, Dmajor, Emajor, Fmajor, Gmajor];
+    playedChords = [];
+}
+
+//Main method
+document.body.style.backgroundColor = `rgb(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)})`;
 input.focus();
 scoreNode.innerHTML = score;
+initializeChords()
 getExercise();
